d206789243cdd45a592abb46128d46e1f9da851c
Chunt0 <chunt.mail@gmail.com>
2024-07-28 22:35:00 -0700

    2024-07-28 22:35:00

--- lua/kickstart/plugins/llm.lua
+++ lua/kickstart/plugins/llm.lua
@@ -97,7 +97,7 @@ return {
 				}, llm.make_ollama_spec_curl_args, llm.handle_ollama_spec_data)
 			end
 
-			vim.keymap.set({ "n", "v" }, "<C-s>", anthropic_help, { desc = "llm ollama help" })
+			vim.keymap.set({ "n", "v" }, "<C-s>", ollama_help, { desc = "llm ollama help" })
 			--vim.keymap.set({ "n", "v" }, "<C-b>", ollama_code, { desc = "llm ollama replace" })
 			--vim.keymap.set({ "n", "v" }, "<leader>K", groq_help, { desc = "llm groq_help" })
 			--vim.keymap.set({ "n", "v" }, "<leader>k", groq_replace, { desc = "llm groq_help" })
