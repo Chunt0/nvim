13044b844ae554080ed3c57953d17dfcc3ca436a
Chunt0 <chunt.mail@gmail.com>
2024-07-29 14:46:12 -0700

    2024-07-29 14:46:12

--- lua/kickstart/plugins/llm.lua
+++ lua/kickstart/plugins/llm.lua
@@ -3,6 +3,7 @@ return {
 		"Chunt0/llm.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			--[[
 			local system_prompt =
 				"You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks"
 			local helpful_prompt =
@@ -11,14 +12,15 @@ return {
 				"You are a helpful assistant. Your goal is to translate text. You will not add anything to the text or output and commentary about the text you generate. Do not add any notes or warnings. Translate the following into chinese: "
 			local en2ar_prompt =
 				"You are a helpful assistant. Your goal is to translate text. You will not add anything to the text or output and commentary about the text you generate. Do not add any notes or warnings. Translate the following into arabic: "
+      ]]
 			local llm = require("llm")
-
+			local prompts = require("prompts")
 			local function groq_replace()
 				llm.invoke_llm_and_stream_into_editor({
 					url = "https://api.groq.com/openai/v1/chat/completions",
 					model = "llama3-70b-8192",
 					api_key_name = "GROQ_API_KEY",
-					system_prompt = system_prompt,
+					system_prompt = prompts.system_prompt,
 					replace = true,
 					framework = "GROQ",
 				}, llm.make_groq_spec_curl_args, llm.handle_groq_spec_data)
@@ -29,7 +31,7 @@ return {
 					url = "https://api.groq.com/openai/v1/chat/completions",
 					model = "llama-3.1-70b-versatile",
 					api_key_name = "GROQ_API_KEY",
-					system_prompt = helpful_prompt,
+					system_prompt = prompts.helpful_prompt,
 					replace = false,
 					framework = "GROQ",
 				}, llm.make_groq_spec_curl_args, llm.handle_groq_spec_data)
@@ -40,7 +42,7 @@ return {
 					url = "https://api.openai.com/v1/chat/completions",
 					model = "gpt-4o",
 					api_key_name = "OPENAI_API_KEY",
-					system_prompt = system_prompt,
+					system_prompt = prompts.system_prompt,
 					replace = true,
 					framework = "OPENAI",
 				}, llm.make_openai_spec_curl_args, llm.handle_openai_spec_data)
@@ -51,7 +53,7 @@ return {
 					url = "https://api.openai.com/v1/chat/completions",
 					model = "gpt-4o",
 					api_key_name = "OPENAI_API_KEY",
-					system_prompt = en2ar_prompt,
+					system_prompt = prompts.en2ar_prompt,
 					replace = false,
 					framework = "OPENAI",
 				}, llm.make_openai_spec_curl_args, llm.handle_openai_spec_data)
@@ -62,7 +64,7 @@ return {
 					url = "https://api.anthropic.com/v1/messages",
 					model = "claude-3-5-sonnet-20240620",
 					api_key_name = "ANTHROPIC_API_KEY",
-					system_prompt = helpful_prompt,
+					system_prompt = prompts.helpful_prompt,
 					replace = false,
 					framework = "ANTHROPIC",
 				}, llm.make_anthropic_spec_curl_args, llm.handle_anthropic_spec_data)
@@ -73,7 +75,7 @@ return {
 					url = "https://api.anthropic.com/v1/messages",
 					model = "claude-3-5-sonnet-20240620",
 					api_key_name = "ANTHROPIC_API_KEY",
-					system_prompt = system_prompt,
+					system_prompt = prompts.system_prompt,
 					replace = true,
 					framework = "ANTHROPIC",
 				}, llm.make_anthropic_spec_curl_args, llm.handle_anthropic_spec_data)
@@ -83,7 +85,7 @@ return {
 				llm.invoke_llm_and_stream_into_editor({
 					url = "http://localhost:11434/api/generate",
 					model = "llama3.1:70b-instruct-q2_k",
-					system_prompt = helpful_prompt,
+					system_prompt = prompts.helpful_prompt,
 					replace = false,
 					context = true,
 					framework = "OLLAMA",
@@ -94,7 +96,7 @@ return {
 				llm.invoke_llm_and_stream_into_editor({
 					url = "http://localhost:11434/api/generate",
 					model = "deepseek-coder:33b",
-					system_prompt = system_prompt,
+					system_prompt = prompts.system_prompt,
 					replace = true,
 					context = false,
 					framework = "OLLAMA",
@@ -105,7 +107,7 @@ return {
 				llm.invoke_llm_and_stream_into_editor({
 					url = "http://localhost:11434/api/generate",
 					model = "yi:34b",
-					system_prompt = en2ch_prompt,
+					system_prompt = prompts.en2ch_prompt,
 					replace = false,
 					context = false,
 					framework = "OLLAMA",
