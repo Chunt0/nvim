db3b7359d2b8081166068e42dac1c1bf3b311377
Chunt0 <chunt.mail@gmail.com>
2024-07-30 15:22:59 -0700

    2024-07-30 15:22:59

--- lua/kickstart/plugins/buftabline.lua
+++ lua/kickstart/plugins/buftabline.lua
@@ -5,6 +5,34 @@ return {
 		config = function()
 			require("buftabline").setup({})
 		end,
+		options = {
+			tab_format = "#{i} #{n}: #{b}#{f} ",
+			buffer_id_index = false,
+			icon_colors = true,
+			start_hidden = false,
+			auto_hide = false,
+			disable_commands = false,
+			go_to_maps = true,
+			flags = {
+				modified = " [+]",
+				not_modifiable = " [-]",
+				readonly = " [RO]",
+			},
+			hlgroups = {
+				current = "TabLineSel",
+				normal = "TabLine",
+				active = nil,
+				spacing = nil,
+				modified_current = nil,
+				modified_normal = nil,
+				modified_active = nil,
+				tabpage_current = nil,
+				tabpage_normal = nil,
+			},
+			show_tabpages = true,
+			tabpage_format = " #{n} ",
+			tabpage_position = "right",
+		},
 	},
 }
 -- vim: ts=2 sts=2 sw=2 et
