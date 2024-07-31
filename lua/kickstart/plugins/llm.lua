6880e90064f23f9229c5989531c6a0b1290f6ed0
Chunt0 <chunt.mail@gmail.com>
2024-07-30 22:30:12 -0700

    2024-07-30 22:30:12

--- lua/kickstart/plugins/llm.lua
+++ lua/kickstart/plugins/llm.lua
@@ -11,14 +11,14 @@ return {
 
 			--vim.keymap.set({ "n", "v" }, "<leader>J", ollama.ch2en, { desc = "llm ollama help" })
 			--vim.keymap.set({ "n", "v" }, "<leader>j", ollama.en2ch, { desc = "llm ollama replace" })
+			vim.keymap.set({ "n", "v" }, "<leader>H", anthropic.help, { desc = "llm anthropic_help" })
+			vim.keymap.set({ "n", "v" }, "<leader>h", anthropic.code, { desc = "llm anthropic" })
 			vim.keymap.set({ "n", "v" }, "<leader>J", perplexity.help, { desc = "llm ollama help" })
-			vim.keymap.set({ "n", "v" }, "<leader>j", ollama.code, { desc = "llm ollama replace" })
+			vim.keymap.set({ "n", "v" }, "<leader>j", perplexity.code, { desc = "llm ollama replace" })
 			vim.keymap.set({ "n", "v" }, "<leader>K", groq.help, { desc = "llm groq_help" })
 			vim.keymap.set({ "n", "v" }, "<leader>k", groq.code, { desc = "llm groq_help" })
 			vim.keymap.set({ "n", "v" }, "<leader>L", openai.help, { desc = "llm openai_help" })
 			vim.keymap.set({ "n", "v" }, "<leader>l", openai.code, { desc = "llm openai" })
-			vim.keymap.set({ "n", "v" }, "<leader>H", anthropic.help, { desc = "llm anthropic_help" })
-			vim.keymap.set({ "n", "v" }, "<leader>h", anthropic.code, { desc = "llm anthropic" })
 		end,
 	},
 }
