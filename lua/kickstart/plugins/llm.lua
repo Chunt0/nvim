5cbcfb6fb83eea74f1f276cb17fe7a74bace2892
Chunt0 <chunt.mail@gmail.com>
2024-08-09 13:21:15 -0700

    2024-08-09 13:21:15

--- lua/kickstart/plugins/llm.lua
+++ lua/kickstart/plugins/llm.lua
@@ -12,18 +12,13 @@ return {
 			local my_prompts = require("custom/my_prompts")
 			local my_models = require("custom/my_models")
 
-			models.perplexity = my_models.perplexity.perplexity_chat
-			models.openai = my_models.openai.gpt_4o
-			models.anthropic = my_models.anthropic.claude_sonnet
-			models.groq = my_models.groq.gemma_2
-
 			prompts.system_prompt = my_prompts.system_prompt
 
 			vim.keymap.set({ "n", "v" }, "<leader>H", anthropic.invoke, { desc = "llm anthropic" })
 			vim.keymap.set({ "n", "v" }, "<leader>J", perplexity.invoke, { desc = "llm perplexity" })
 			vim.keymap.set({ "n", "v" }, "<leader>K", groq.invoke, { desc = "llm groq" })
 			vim.keymap.set({ "n", "v" }, "<leader>L", openai.invoke, { desc = "llm openai" })
-			vim.keymap.set({ "n", "v" }, "<leader>l", openai.code, { desc = "llm openai code" })
+			vim.keymap.set({ "n", "v" }, "<leader>l", openai.dalle, { desc = "llm openai code" })
 		end,
 	},
 }
