12a224e2082ed458d9c7b5dcde8c707e72cb8faf
Chunt0 <chunt.mail@gmail.com>
2024-08-14 11:01:31 -0700

    2024-08-14 11:01:31

--- lua/kickstart/plugins/llm.lua
+++ lua/kickstart/plugins/llm.lua
@@ -14,14 +14,14 @@ return {
 			local vars = require("variables")
 
 			-- Example use of models
-			models.openai = my_models.openai.gpt_4o_mini -- Use gpt-4o-mini instead of default gpt-4o
+			-- models.openai = my_models.openai.gpt_4o_mini -- Use gpt-4o-mini instead of default gpt-4o
 			-- models.groq = my_models.groq.mixtral_8x7b -- Use mixtral_8x7b instead of default llama3.1-70b-versatile
 
 			-- Example use of system_prompt set up
 			prompts.system_prompt = my_prompts.faithful_prompt
 
 			-- Example use of vars
-			vars.temp = 1.5 -- value between 0 - 2 default is 0.7, increases randomness in token sampling. Higher values create greater randomness.
+			-- vars.temp = 1.5 -- value between 0 - 2 default is 0.7, increases randomness in token sampling. Higher values create greater randomness.
 			-- vars.top_p = 0.5 -- value between 0 - 1 default is 1, determines the range of possible tokens to be sampled from. A value less than 1 reduces the space of possible tokens to be sampled
 			-- vars.presence_penalty =  -- value between -2 - 2  default is 0, a higher value increases penalty for repeating previously produced tokens
 
