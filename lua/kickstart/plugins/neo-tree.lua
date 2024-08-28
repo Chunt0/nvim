4ddb7e92dedcf93886b6c28ad34a744f7c5a8253
Chunt0 <chunt.mail@gmail.com>
2024-08-28 10:49:31 -0700

    2024-08-28 10:49:31

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,28 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal" },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				hide_dotfiles = false,
+				hide_gitignore = false,
+			},
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
 }
