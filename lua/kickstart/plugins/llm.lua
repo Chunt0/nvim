4dc876e4ca27daf02093fd907a94624760d37e7f
Chunt0 <chunt.mail@gmail.com>
2025-01-01 19:50:02 -0800

    2025-01-01 19:50:02

--- lua/kickstart/plugins/llm.lua
+++ lua/kickstart/plugins/llm.lua
@@ -12,24 +12,21 @@ return {
 			-- local perplexity = require("perplexity")
 
 			-- Import configurable params
-			local prompts = require("prompts")
 			local my_prompts = require("custom/my_prompts")
-			local models = require("models")
 			local my_models = require("custom/my_models")
-			local vars = require("variables")
+			local constants = require("constants")
 
 			-- Example use of models
-			-- models.openai = my_models.openai.o1_mini -- Use gpt-4o-mini instead of default gpt-4o
-			models.anthropic = my_models.anthropic.calude_haiku
-			-- models.groq = my_models.groq.mixtral_8x7b -- Use mixtral_8x7b instead of default llama3.1-70b-versatile
+			-- constants.models.openai = my_models.openai.o1_mini -- Use gpt-4o-mini instead of default gpt-4o
+			-- constants.models.groq = my_models.groq.mixtral_8x7b -- Use mixtral_8x7b instead of default llama3.1-70b-versatile
 
 			-- Example use of system_prompt set up
-			-- prompts.system_prompt = my_prompts.helpful_prompt
+			-- constants.prompts.system_prompt = my_prompts.helpful_prompt
 
 			-- Example use of vars
-			-- vars.temp = 1.5 -- value between 0 - 2 default is 0.7, increases randomness in token sampling. Higher values create greater randomness.
-			-- vars.top_p = 0.5 -- value between 0 - 1 default is 1, determines the range of possible tokens to be sampled from. A value less than 1 reduces the space of possible tokens to be sampled
-			-- vars.presence_penalty =  -- value between -2 - 2  default is 0, a higher value increases penalty for repeating previously produced tokens
+			-- constants.vars.temp = 1.5 -- value between 0 - 2 default is 0.7, increases randomness in token sampling. Higher values create greater randomness.
+			-- constants.vars.top_p = 0.5 -- value between 0 - 1 default is 1, determines the range of possible tokens to be sampled from. A value less than 1 reduces the space of possible tokens to be sampled
+			-- constants.vars.presence_penalty =  -- value between -2 - 2  default is 0, a higher value increases penalty for repeating previously produced tokens
 
 			-- Clear message buffer
 			vim.keymap.set(
